da15a61c2626293f7d0d1884f5ec8ae60e68d23c
Xiao Yu <xy2437@cunixprod01.cc.columbia.edu>
2021-01-24 00:55:54 -0500

    People Update 1.0 Done
    - fixed relative path issues
    - fixed image name conversion
    
    TODO: add other people who handed bio in late

--- scripts/event_template.js
+++ scripts/event_template.js
@@ -6,13 +6,12 @@ const timestamp = urlParams.get('timestamp');
 
 var event_list;
 // changed path due to deployment
-fetch('assets/sample_events.json')
+fetch('/cu/gcc/assets/sample_events.json')
 .then(res => res.json())
 .then(data => {
     event_list = data;
     create_event_page(data[timestamp]);
-})
-.catch(err => console.log("fetch json failed", err))
+}).catch(err => console.log("fetch json failed", err))
 
 function get_latest_event() {
     let events = [];
@@ -50,9 +49,9 @@ function create_event_page(info){
     var latest_timestamps = get_latest_event();
     for(let i=0; i<latest_projects.children.length; i++){
         var project = latest_projects.children[i]; // gets the div "<div class="col-md-4 item">"
-        project.children[0].href = `./portfolio-single.html?timestamp=${latest_timestamps[i]}`;
+        project.children[0].href = `/cu/gcc/gcc_events/portfolio-single.html?timestamp=${latest_timestamps[i]}`;
         project.children[0].children[0].src = event_list[latest_timestamps[i]].thumb_nail;
-        project.children[1].href = `./portfolio-single.html?timestamp=${latest_timestamps[i]}`;
+        project.children[1].href = `/cu/gcc/gcc_events/portfolio-single.html?timestamp=${latest_timestamps[i]}`;
         project.children[1].children[0].textContent = event_list[latest_timestamps[i]].title;
     }
 }
