25d7ff7d71a4d2c2954a8eb430187abaf70f5cce
Xiao Yu <jasonyux17@gmail.com>
2021-10-09 17:06:42 -0400

    added space

--- scripts/profile_generator.js
+++ scripts/profile_generator.js
@@ -1,8 +1,8 @@
 export default function insert_profiles(containerID, list, wrapper) {
-  var teamGridDiv = document.getElementById(containerID);
-  for(let info of list){
-      teamGridDiv.appendChild(wrapper(info));
-  }
+    var teamGridDiv = document.getElementById(containerID);
+    for (let info of list) {
+        teamGridDiv.appendChild(wrapper(info));
+    }
 }
 
 export const production_root = '/cu/gcc/'
